8cd0a0b3e415ac129da8f0088a6152278531e662
pham-tan <45042869+pham-tan@users.noreply.github.com>
2018-12-20 20:20:37 +0700

    add

--- includes/footer.php
+++
@@ -1,9 +0,0 @@
-<div class="clr"></div>
-        <div class="bootstrap-iso">
-            <div class="col-lg-12 text-center" style="background:antiquewhite;">
-                <p >phamtan365@gmail.com</p>
-            </div>
-        </div>
-    </div>
-</body>
-</html>
--- includes/menu_login/chuadangnhap.php
+++
@@ -1,2 +0,0 @@
-<div class="register"><a href="./dangki.php" title="Đăng ký">Đăng ký</a></div>
-<div class="login"><a href="./dangnhap.php" title="Đăng nhập">Đăng nhập</a></div>
